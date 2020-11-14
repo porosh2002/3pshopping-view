153e7342acb6ccc511371cbb8ab9ea36f66732cc
porosh2002 <jamilkashem2002@gmail.com>
2020-11-14 10:22:18 +0600

    a

--- Client/src/Components/Navigation/NavDesk.js
+++ Client/src/Components/Navigation/NavDesk.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import logo from '../../Images/logo.png'
+import logo from '../../Images/logo.png';
+import {Link} from 'react-router-dom'
 export default function NavDesk() {
     return (
         <div className='navdesk'>
+        <Link className='logo' to='/'>
             <img src={logo} alt='Hq Bet 365' />
+        </Link>   
+        <ul className='nav-ul'>
+            <li><Link className='menu-Link' to=''>Home</Link></li>
+            <li><Link className='menu-Link' to=''>Home</Link></li>
+            <li><Link className='menu-Link' to=''>Home</Link></li>
+            <li><Link className='menu-Link' to=''>Home</Link></li>
+        </ul>
         </div>
     )
 }
