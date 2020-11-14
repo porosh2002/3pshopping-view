7d52623e60380cebbb2f9194ac3d97ae635713c5
porosh2002 <jamilkashem2002@gmail.com>
2020-11-14 10:35:57 +0600

    test

--- Client/src/Components/Navigation/NavDesk.js
+++ Client/src/Components/Navigation/NavDesk.js
@@ -4,14 +4,14 @@ import {Link} from 'react-router-dom'
 export default function NavDesk() {
     return (
         <div className='navdesk'>
-        <Link className='logo' to='/'>
+        <Link to='/'>
             <img src={logo} alt='Hq Bet 365' />
         </Link>   
         <ul className='nav-ul'>
             <li><Link className='menu-Link' to=''>Home</Link></li>
-            <li><Link className='menu-Link' to=''>Home</Link></li>
-            <li><Link className='menu-Link' to=''>Home</Link></li>
-            <li><Link className='menu-Link' to=''>Home</Link></li>
+            <li><Link className='menu-Link' to=''>Live Match</Link></li>
+            <li><Link className='menu-Link' to=''>Upcoming Match</Link></li>
+            <li><Link className='menu-Link' to='/login'>Login/Signup</Link></li>
         </ul>
         </div>
     )
