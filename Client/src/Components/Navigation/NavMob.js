def6e32719b981c70c14cb02c222010593e6812e
porosh2002 <jamilkashem2002@gmail.com>
2020-11-14 16:58:25 +0600

    major changes made

--- Client/src/Components/Navigation/NavMob.js
+++ Client/src/Components/Navigation/NavMob.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../Images/logo.png";
+import { FaBars,FaTimes} from "react-icons/fa";
 
-export default function NavMob() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class NavMob extends Component {
+  state={
+    modalOpen:false,
+  }
+  makemodalOpenFalse=()=>{
+    this.setState({modalOpen:false})
+  }
+    render() {     
+      const {modalOpen} = this.state;  
+      const style = modalOpen ? (null):({ display:"none" }) ;
+      return (
+<div>
+<div className="navmob">
+            <Link to="/">
+              <img className="logo" src={logo} alt="Hq Bet 365" />
+            </Link>
+            <div className="bars">
+            {this.state.modalOpen ? (<FaTimes  onClick={this.makemodalOpenFalse}/>):(<FaBars onClick={()=>{this.setState({modalOpen:true})}}/>)}
+            </div>
+          </div>
+          <div style={style} className='nav-modal-mob'>
+          <ul className='nav-ul ab-p-60'>
+            <li><Link onClick={this.makemodalOpenFalse} className='menu-Link' to=''>Home</Link></li>
+            <li><Link onClick={this.makemodalOpenFalse} className='menu-Link' to=''>Live Match</Link></li>
+            <li><Link onClick={this.makemodalOpenFalse} className='menu-Link' to=''>Upcoming Match</Link></li>
+            <li><Link onClick={this.makemodalOpenFalse} className='menu-Link' to='/login'>Login/Signup</Link></li>
+        </ul>
+          </div>
+</div>
+        )
+    }
 }
