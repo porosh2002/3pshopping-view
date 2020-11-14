6b6f1b6645ee5e0bb94469e311ed5de818765ab0
porosh2002 <jamilkashem2002@gmail.com>
2020-11-14 19:41:45 +0600

    swiper deleted and node-sass

--- Client/src/Components/Slider/Slider.js
+++ Client/src/Components/Slider/Slider.js
@@ -1,35 +1,9 @@
-import React, { Component } from "react";
-import SwiperCore, { Autoplay , Navigation} from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-SwiperCore.use([Autoplay,Navigation]);
-export default class Slider extends Component {
-  Scroll = () => {
-    if (window.innerWidth > 900) {
-      return 4;
-    } else {
-      return 2;
-    }
-  };
-  render() {
-    return (
-<div>
-<Swiper navigation
-        spaceBetween={30}
-        slidesPerView='1'
-        loop={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-      >
- <SwiperSlide>
+import React from 'react'
 
-        </SwiperSlide>
-                <SwiperSlide>
+export default function Slider() {
+  return (
+<div className='slider-image'>
 
-        </SwiperSlide>
-      </Swiper>
 </div>
-    );
-  }
+  )
 }
