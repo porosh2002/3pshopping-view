a222d20ccd073c3993da84c21bc967f3c7e1a483
porosh2002 <jamilkashem2002@gmail.com>
2020-11-15 16:49:42 +0600

    slider

--- Client/src/Components/Slider/Slider.js
+++ Client/src/Components/Slider/Slider.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function Slider() {
   return (
 <div className='slider-image'>
-
+<div className='slider'>
+  <div className='slider-txt-wraper'>
+<p className='slider-txt'>Get up to 15x extra for winning a Bet</p>
+<Link className='cta-btn' to='/'>Bet Now</Link>
+  </div>
+</div>
 </div>
   )
 }
