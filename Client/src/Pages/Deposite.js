6d5837eee3e9b22ed78c44fe60c2a16e70b4875f
porosh2002 <jamilkashem2002@gmail.com>
2020-11-22 04:15:22 +0600

    test

--- Client/src/Pages/Deposite.js
+++ Client/src/Pages/Deposite.js
@@ -14,8 +14,9 @@ export default class Deposite extends PureComponent {
             <div>
                 <Menu />
                 {this.state.data.map((data,i)=>{
-                    return <div key={i} style={{display:"inline-block",margin:"30px",fontSize:"20px"}}>
+                    return <div key={i} style={{display:"block",margin:"30px",fontSize:"20px"}}>
                         <p style={{margin:"30px",display:"inline-block"}}>{data.userid}</p> <p style={{margin:"30px",display:"inline-block"}}>{data.tid}</p>
+                        <div style={{margin:"30px",borderRadius:"3px",display:"inline-block",padding:"10px 7px",backgroundColor:"#444",color:"#f7f7f7",cursor:"pointer"}}>Approve</div>
                     </div>
                 })}
             </div>
