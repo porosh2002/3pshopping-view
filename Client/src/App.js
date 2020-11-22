d01aa02d3ed3ab0b7de495bfcc8a06c23d4499ab
porosh2002 <jamilkashem2002@gmail.com>
2020-11-22 22:25:37 +0600

    admin route

--- Client/src/App.js
+++ Client/src/App.js
@@ -22,7 +22,6 @@ class App extends Component {
 
     // !!!!!!!!!! 
     const { userID,adminID } = this.props;
-    console.log(adminID);
     // !!!!!!!!!! 
 
     return (
@@ -33,15 +32,15 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/Login" render={()=>userID !== undefined ? (<Redirect to='/user' />):(<Login />)} />
             <Route exact path="/Signup" component={Signup} />
-            <Route exact path="/admin" component={Admin} />
+            <Route exact path="/admin" render={()=>adminID === false ? (<Redirect to='/with_wecubs' />):(<Admin />)} />
             <Route exact path="/user" render={()=>userID === undefined ? (<Redirect to='/login' />):(<User />)} />
             <Route exact path="/football" component={Football} />
             <Route exact path="/cricket" component={Cricket} />
             <Route exact path="/tennis" component={Tennis} />
-            <Route exact path="/payment" component={Payment} />
-            <Route exact path="/deposite" component={Deposite} />
-            <Route exact path="/result" component={MatchResult} />
-            <Route exact path="/with_wecubs" component={AdminCheck} />
+            <Route exact path="/payment"  render={()=>adminID === false ? (<Redirect to='/with_wecubs' />):(<Payment />)} />
+            <Route exact path="/deposite"  render={()=>adminID === false ? (<Redirect to='/with_wecubs' />):(<Deposite />)} />
+            <Route exact path="/result"  render={()=>adminID === false ? (<Redirect to='/with_wecubs' />):(<MatchResult />)} />
+            <Route exact path="/with_wecubs" render={()=>adminID === true ? (<Redirect to='/admin' />):(<AdminCheck />)} />
             <Route component={Error} />
           </Switch>
         </Suspense>
