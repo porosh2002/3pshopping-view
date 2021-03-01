3c14752bd580150bde558fab943152bb70ebee73
porosh <jamilkashem2002@gmail.com>
2021-03-01 08:27:22 +0600

    Browse UI updated

--- Client/src/Pages/BrowseMovie.js
+++ Client/src/Pages/BrowseMovie.js
@@ -7,8 +7,8 @@ export default class BrowseMovie extends Component {
             <p className='BrowseTitle'>Search : </p>
             <input placeholder='name' spellCheck='false' autoFocus className='BrowseSearchBar' type='text'></input>
 <div className='SelectsBrowse'>
-<div>
-<p className='BrowseTitle'>Search : </p>
+<div className='SelectWraper'>
+<p className='BrowseTitle'>Genre : </p>
 <select>
                 <option>one</option>
                 <option>one</option>
@@ -16,8 +16,26 @@ export default class BrowseMovie extends Component {
                 <option>one</option>
             </select>
 </div>
-<div>
-<p className='BrowseTitle'>Search : </p>
+<div className='SelectWraper'>
+<p className='BrowseTitle'>Language : </p>
+            <select>
+                <option>one</option>
+                <option>one</option>
+                <option>one</option>
+                <option>one</option>
+            </select>
+</div>
+<div className='SelectWraper'>
+<p className='BrowseTitle'>Rating : </p>
+            <select>
+                <option>one</option>
+                <option>one</option>
+                <option>one</option>
+                <option>one</option>
+            </select>
+</div>
+<div className='SelectWraper'>
+<p className='BrowseTitle'>Year : </p>
             <select>
                 <option>one</option>
                 <option>one</option>
