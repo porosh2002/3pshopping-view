1f395b2a3fc05bd1dcef704cb6ac8ccdf26bfa67
porosh <jamilkashem2002@gmail.com>
2021-03-02 15:33:06 +0600

    Admin Auth

--- Client/src/App.js
+++ Client/src/App.js
@@ -10,6 +10,8 @@ const Upload = React.lazy(() => import("./Pages/Upload"));
 const Login = React.lazy(() => import("./Pages/Login"));
 const Signup = React.lazy(() => import("./Pages/Signup"));
 const Request = React.lazy(() => import("./Pages/Request"));
+const ImageUpload = React.lazy(() => import("./Pages/ImageUpload"));
+const AuthAdmin = React.lazy(() => import("./Pages/auth.admin"));
 
 class App extends Component {
   render() {
@@ -19,10 +21,12 @@ class App extends Component {
         <Suspense fallback={<Loading />}>
        <Switch>
        <Route exact path="/" component={Home}/>
+       <Route exact path="/auth" component={AuthAdmin}/>
        <Route exact path="/browse" component={BrowseMovie}/>
-       <Route exact path="/upload" component={Upload}/>
+       <Route exact path="/upload/movie" component={Upload}/>
        <Route exact path="/login" component={Login}/>
        <Route exact path="/Request" component={Request}/>
+       <Route exact path="/content/Image" component={ImageUpload}/>
        <Route exact path="/signup" component={Signup}/>
        <Route component={ErrorPage}/>
       </Switch>
---
+++ Client/src/Pages/ImageUpload.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ImageUpload extends Component {
+    render() {
+        return (
+            <div>
+                Image Upload
+            </div>
+        )
+    }
+}
