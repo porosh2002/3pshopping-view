dfe948c4e9aadd196ecb55f05c691c763ae32156
porosh2002 <jamilkashem2002@gmail.com>
2021-04-08 09:11:27 +0600

    Catagories is now updating

--- Client/src/Pages/Upload.js
+++ Client/src/Pages/Upload.js
@@ -19,7 +19,7 @@ class Upload extends Component {
     Description: null,
     Download_Link: null,
     Subtitle_Link: null,
-    Action: false,
+    Action:false,
     Adventure: false,
     Animation: false,
     Biography: false,
@@ -40,18 +40,38 @@ class Upload extends Component {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
+  CategoriesSelect = (e) => {
+const name = e.target.attributes.name.value
+    this.setState({ [name]: !this.state.name });
+  };
   CurrentTime = () => {
     return moment().format("MMMM Do YYYY, h:mm a");
   };
   render() {
-    console.log(this.state);
+    const {
+      Action,
+      Adventure,
+      Animation,
+      Biography,
+      Thriller,
+      Comedy,
+      Crime,
+      Documentary,
+      Drama,
+      Family,
+      Fantasy,
+      Horror,
+      Mystery,
+      Romance,
+    } = this.state; 
     return (
       <div className="UploadDiv">
         <p className="MainTitleUp">Welcome to Admin Panel</p>
         <form>
           <div className="UploadContentWraper">
             <p className="uploadTitle">Movie name : </p>
-            <Form onChange={this.FormValueChange}
+            <Form
+              onChange={this.FormValueChange}
               type="text"
               placeholder="movie name"
               name="Movie_Name"
@@ -61,7 +81,8 @@ class Upload extends Component {
           </div>
           <div className="UploadContentWraper">
             <p className="uploadTitle">Year : </p>
-            <Form onChange={this.FormValueChange}
+            <Form
+              onChange={this.FormValueChange}
               type="number"
               placeholder="movie relese year"
               name="Movie_Year"
@@ -77,11 +98,17 @@ class Upload extends Component {
           </div>
           <div className="UploadContentWraper">
             <p className="uploadTitle">IMDB Rating : </p>
-            <Form onChange={this.FormValueChange} type="number" placeholder="IMDB Rating" name="IMDB_Rating" />
+            <Form
+              onChange={this.FormValueChange}
+              type="number"
+              placeholder="IMDB Rating"
+              name="IMDB_Rating"
+            />
           </div>
           <div className="UploadContentWraper">
             <p className="uploadTitle">Rotten Tomatoes Rating : </p>
-            <Form onChange={this.FormValueChange}
+            <Form
+              onChange={this.FormValueChange}
               type="number"
               placeholder="Rotten Tomatoes Rating"
               name="Tomatos_Rating"
@@ -89,7 +116,8 @@ class Upload extends Component {
           </div>
           <div className="UploadContentWraper">
             <p className="uploadTitle"> Metacritic Rating : </p>
-            <Form onChange={this.FormValueChange}
+            <Form
+              onChange={this.FormValueChange}
               type="number"
               placeholder=" Metacritic Rating"
               name="Metacritic_Rating"
@@ -97,15 +125,26 @@ class Upload extends Component {
           </div>
           <div className="UploadContentWraper">
             <p className="uploadTitle">Minimum Age Requirement : </p>
-            <Form onChange={this.FormValueChange} type="number" placeholder="12" name="Age_Requirement" />
+            <Form
+              onChange={this.FormValueChange}
+              type="number"
+              placeholder="12"
+              name="Age_Requirement"
+            />
           </div>
           <div className="UploadContentWraper">
             <p className="uploadTitle">Trailer Link : </p>
-            <Form onChange={this.FormValueChange} type="text" placeholder="Trailer Link" name="Trailer_Link" />
+            <Form
+              onChange={this.FormValueChange}
+              type="text"
+              placeholder="Trailer Link"
+              name="Trailer_Link"
+            />
           </div>
           <div className="UploadContentWraper">
             <p className="uploadTitle">Director : </p>
-            <Form onChange={this.FormValueChange}
+            <Form
+              onChange={this.FormValueChange}
               type="text"
               placeholder="Director name"
               name="Director_Link"
@@ -114,27 +153,119 @@ class Upload extends Component {
           {/* Cetagories */}
           <div>
             <p className="uploadTitle">Categories</p>
-            <p className="categories">Action</p>
-            <p className="categories">Adventure</p>
-            <p className="categories">Animation</p>
-            <p className="categories">Family</p>
-            <p className="categories">Biography</p>
-            <p className="categories">Thriller</p>
-            <p className="categories">Crime</p>
-            <p className="categories">Superhero</p>
-            <p className="categories">Comedy</p>
-            <p className="categories">Documentary</p>
-            <p className="categories">Drame</p>
-            <p className="categories">Fantasy</p>
-            <p className="categories">Romance</p>
-            <p className="categories">Horror</p>
-            <p className="categories">Mystery</p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Action"
+              style={Action ? {backgroundColor:"#00fa9a"} :null}
+            >
+              Action
+            </p>
+            <p
+              className="categories"
+              name="Adventure"
+              onClick={this.CategoriesSelect}
+            >
+              Adventure
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Animation"
+            >
+              Animation
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Family"
+            >
+              Family
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Biography"
+            >
+              Biography
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Thriller"
+            >
+              Thriller
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Crime"
+            >
+              Crime
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Superhero"
+            >
+              Superhero
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Comedy"
+            >
+              Comedy
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Documentary"
+            >
+              Documentary
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Drame"
+            >
+              Drame
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Fantasy"
+            >
+              Fantasy
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Romance"
+            >
+              Romance
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Horror"
+            >
+              Horror
+            </p>
+            <p
+              className="categories"
+              onClick={this.CategoriesSelect}
+              name="Mystery"
+            >
+              Mystery
+            </p>
           </div>
           <br></br>
           {/* TextArea */}
           <div>
             <p className="uploadTitle">Casts : </p>
-            <textarea onChange={this.FormValueChange}
+            <textarea
+              onChange={this.FormValueChange}
               spellCheck="false"
               className="UploadPageTextArea"
               name="Casts"
@@ -144,7 +275,8 @@ class Upload extends Component {
           <br></br>
           <div>
             <p className="uploadTitle">Description : </p>
-            <textarea onChange={this.FormValueChange}
+            <textarea
+              onChange={this.FormValueChange}
               spellCheck="false"
               className="UploadPageTextArea"
               name="Description"
@@ -154,7 +286,8 @@ class Upload extends Component {
           <br></br>
           <div>
             <p className="uploadTitle">Download Links : </p>
-            <textarea onChange={this.FormValueChange}
+            <textarea
+              onChange={this.FormValueChange}
               spellCheck="false"
               className="UploadPageTextArea"
               name="Download_Link"
@@ -164,7 +297,8 @@ class Upload extends Component {
           <br></br>
           <div>
             <p className="uploadTitle">Subtitle Download Links : </p>
-            <textarea onChange={this.FormValueChange}
+            <textarea
+              onChange={this.FormValueChange}
               spellCheck="false"
               className="UploadPageTextArea"
               name="Subtitle_Link"
